2503eb8f2f2ee94e600c20e833e518f89dc99e40
chiuzy <razvanandrei293@gmail.com>
2026-06-12 15:56:21 +0300

    feat: add Notes app with localStorage persistence

--- js/apps.js
+++ js/apps.js
@@ -1,8 +1,9 @@
 import { terminalApp } from './apps/terminal.js';
 import { filesApp } from './apps/files.js';
 import { aboutApp } from './apps/about.js';
+import { notesApp } from './apps/notes.js';
 
-export const apps = [terminalApp, filesApp, aboutApp];
+export const apps = [terminalApp, filesApp, aboutApp, notesApp];
 
 export function getApp(id) {
   return apps.find((a) => a.id === id);
